a117e5d12c85f0d9f3310c457868a452128bbbb1
Eric Keeton <142048130+EMK2473@users.noreply.github.com>
2024-02-13 18:56:03 -0500

    troubleshooting deployment

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,10 @@
-// Import necessary modules from React and ReactDOM
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 
-// Import the main App component
 import App from './App.jsx'
 
-// Import styles for the application
 import './index.css'
 
-// Render the App component using ReactDOM.createRoot()
 ReactDOM.createRoot(document.getElementById('root')).render(
   // Wrap the App component with <React.StrictMode> for additional checks and warnings
   <React.StrictMode>
