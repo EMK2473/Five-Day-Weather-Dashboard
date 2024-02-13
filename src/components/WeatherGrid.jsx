3d30837e6e55660bbae851124840db14441477d2
Eric Keeton <142048130+EMK2473@users.noreply.github.com>
2024-02-13 16:49:12 -0500

    added extra data rows for current forecast

--- src/components/WeatherGrid.jsx
+++ src/components/WeatherGrid.jsx
@@ -1,6 +1,4 @@
-// WeatherGrid.jsx
-
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 
 function WeatherGrid({ city }) {
   const [weatherData, setWeatherData] = useState(null);
@@ -8,11 +6,13 @@ function WeatherGrid({ city }) {
   useEffect(() => {
     const fetchWeatherData = async () => {
       try {
-        const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=1e27d7859898089c77e02a338285d98b&units=metric`);
+        const response = await fetch(
+          `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=1e27d7859898089c77e02a338285d98b&units=metric`
+        );
         const data = await response.json();
         setWeatherData(data);
       } catch (error) {
-        console.error('Error fetching weather data:', error);
+        console.error("Error fetching weather data:", error);
       }
     };
 
@@ -25,10 +25,25 @@ function WeatherGrid({ city }) {
     return <div>Loading...</div>;
   }
 
+  const sunriseTime = new Date(weatherData.sys.sunrise * 1000).toLocaleTimeString(
+    "en-US"
+  );
+  const sunsetTime = new Date(weatherData.sys.sunset * 1000).toLocaleTimeString(
+    "en-US"
+  );
+
   return (
-    <div style={{ border: '1px solid #ccc', padding: '10px', marginBottom: '20px' }}>
-      <h2 style={{ marginBottom: '10px' }}>{city} </h2>
+    <div
+      style={{
+        border: "1px solid #ccc",
+        padding: "10px",
+        marginBottom: "20px",
+        display: "flex",
+        justifyContent: "space-between",
+      }}
+    >
       <div>
+        <h2>{city}</h2>
         <p>Temperature: {weatherData.main.temp}°C</p>
         <p>Wind: {weatherData.wind.speed} m/s</p>
         <p>Humidity: {weatherData.main.humidity}%</p>
@@ -36,6 +51,12 @@ function WeatherGrid({ city }) {
           src={`https://openweathermap.org/img/wn/${weatherData.weather[0].icon}.png`}
           alt={weatherData.weather[0].description}
         />
+      </div>
+      <div style={{marginTop: "25px", marginRight: "300px"}}>
+        <p>Pressure: {weatherData.main.pressure} hPa</p>
+        <p>Visibility: {weatherData.visibility} meters</p>
+        <p>Sunrise: {sunriseTime}</p>
+        <p>Sunset: {sunsetTime}</p>
       </div>
     </div>
   );
